bc0fc9c0db169fbc02cce066808d461a055611bf
Seif Wael <seifeldin7666@gmail.com>
2022-12-09 00:35:27 +0200

    s

--- det3d/core/sampler/preprocess.py
+++ det3d/core/sampler/preprocess.py
@@ -93,6 +93,7 @@ class DBFilterByMinNumPoint(DataBasePreprocessing):
         for name, min_num in self._min_gt_point_dict.items():  # [Car, 5]
             if min_num > 0:
                 filtered_infos = []
+                print(f'Name is {name}')
                 for info in db_infos[name]:
                     if info["num_points_in_gt"] >= min_num:
                         filtered_infos.append(info)
