72f3d25e698cc691c8cd76fd10488f6308fabe66
Seif Wael <seifeldin7666@gmail.com>
2022-12-13 13:29:00 +0200

    Printing X instead

--- det3d/models/readers/voxel_encoder.py
+++ det3d/models/readers/voxel_encoder.py
@@ -105,7 +105,7 @@ class VoxelFeatureExtractor(nn.Module):
         x = F.relu(x)
         x *= mask
         # x: [concated_num_points, num_voxel_size, 128]
-        print(f'Output shape is {features.shape}')
+        print(f'Output shape is {x.shape}')
         voxelwise = torch.max(x, dim=1)[0]
         print(f'Voxelwise is {voxelwise}')
         return voxelwise
