db2716f8cbed8ce2567e13d093baae702e6c9e96
Seif Wael <seifeldin7666@gmail.com>
2022-12-13 14:01:08 +0200

    May concatenating work

--- det3d/models/readers/voxel_encoder.py
+++ det3d/models/readers/voxel_encoder.py
@@ -104,11 +104,12 @@ class VoxelFeatureExtractor(nn.Module):
         x = F.relu(x)
         x *= mask
         # x: [concated_num_points, num_voxel_size, 128]
-        print(f'Output shape is {x.shape}')
-        voxelwise = torch.max(x, dim=1)
-        print(f'Voxelwise shape before concatencaing is {voxelwise}')
-        voxelwise=torch.cat([x, voxelwise], dim=-1)
-        print(f'Voxelwise is {voxelwise.shape}')
+        # print(f'Output shape is {x.shape}')
+        voxelwise = torch.max(x, dim=1)[0]
+        vv= torch.ones((x.shape[0],num_voxels,x.shape[-1]))
+        # print(f'Voxelwise shape before concatencaing is {voxelwise}')
+        voxelwise=torch.cat([x, vv*voxelwise], dim=-1)
+        # print(f'Voxelwise is {voxelwise.shape}')
         return  voxelwise
 
 
